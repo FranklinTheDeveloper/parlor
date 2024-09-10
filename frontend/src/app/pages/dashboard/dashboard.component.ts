2c8f160027ae9bb702559e9f6515fcf61d2ff59c
systems <systems@shearwatervf.com>
2024-09-10 23:48:56 +0200

    done wih registered clients

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, type OnInit } from '@angular/core';
+import { ClientRegistration } from '../../tools/models/registration.interface';
+import { ClientRegistrationService } from '../../tools/services/registration.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,6 +14,19 @@ import { Component, type OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  ngOnInit(): void { }
+  clients: ClientRegistration[] = [];
+
+  NoOfClients: number = 0;
+
+  constructor(private clientsService: ClientRegistrationService) { }
+
+  ngOnInit(): void { 
+    this.clientsService.getClients()
+    .subscribe((res: any) => {
+      this.clients = res.data;
+      console.log('Clients:', this.clients);
+      this.NoOfClients = this.clients.length;
+    })
+  }
 
 }
